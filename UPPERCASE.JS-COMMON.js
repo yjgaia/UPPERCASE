4769ff438ccb4b614834b60017f2d4f8dc99a32a
Hanul <hanul@hanul.me>
2015-04-29 02:51:19 +0900

    add CREATE_DATE

--- UPPERCASE.JS-COMMON.js
+++ UPPERCASE.JS-COMMON.js
@@ -2369,6 +2369,54 @@ global.CALENDAR = CLASS({
 });
 
 /**
+ * create date type.
+ */
+global.CREATE_DATE = METHOD({
+
+	run : function(params) {
+		'use strict';
+		//REQUIRED: params
+		//OPTIONAL: params.year
+		//OPTIONAL: params.month
+		//OPTIONAL: params.date
+		//OPTIONAL: params.hour
+		//OPTIONAL: params.minute
+		//OPTIONAL: params.second
+		
+		var
+		// year
+		year = params.year,
+		
+		// month
+		month = params.month,
+		
+		// date
+		date = params.date,
+		
+		// hour
+		hour = params.hour,
+		
+		// minute
+		minute = params.minute,
+		
+		// second
+		second = params.second,
+		
+		// now cal
+		nowCal = CALENDAR(new Date());
+
+		return new Date(
+			year === undefined ? nowCal.getYear() : year,
+			(month === undefined ? nowCal.getYear() : month) - 1,
+			date === undefined ? nowCal.getDate() : date,
+			hour === undefined ? nowCal.getHour() : hour,
+			minute === undefined ? nowCal.getMinute() : minute,
+			second === undefined ? nowCal.getSecond() : second
+		);
+	}
+});
+
+/**
  * Delay class
  */
 global.DELAY = CLASS({
