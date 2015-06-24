ed3a548e7c7ff6fd1fe1614e380310f5ed05015f
Hanul <hanul@hanul.me>
2015-06-24 18:59:21 +0900

    검색 엔진이 찾을 수 있도록 버그 개선

--- UPPERCASE.IO-BROWSER-PACK/IMPORT.js
+++ UPPERCASE.IO-BROWSER-PACK/IMPORT.js
@@ -5315,12 +5315,17 @@ global.EVENT = CLASS(function(cls) {
 						top = e.getTop();
 
 						if (startLeft - 5 <= left && left <= startLeft + 5 && startTop - 5 <= top && top <= startTop + 5) {
-							
-							if (nodeId !== 'body') {
-								e.stopDefault();
+						
+							if (lastTapTime === undefined || Date.now() - lastTapTime > 100) {
+								
+								lastTapTime = Date.now();
+								
+								if (nodeId !== 'body') {
+									e.stopDefault();
+								}
+								
+								return eventHandler(e, node);
 							}
-							
-							return eventHandler(e, node);
 						}
 					}
 				}));
@@ -5331,13 +5336,19 @@ global.EVENT = CLASS(function(cls) {
 					lowNode : lowNode,
 					name : 'click'
 				}, function(e, node) {
+					
 					if (INFO.checkIsTouchMode() !== true || INFO.checkIsExistsTapDelay() !== true) {
 						
-						if (nodeId !== 'body') {
-							e.stopDefault();
+						if (lastTapTime === undefined || Date.now() - lastTapTime > 100) {
+							
+							lastTapTime = Date.now();
+							
+							if (nodeId !== 'body') {
+								e.stopDefault();
+							}
+							
+							return eventHandler(e, node);
 						}
-						
-						return eventHandler(e, node);
 					}
 				}));
 			}
