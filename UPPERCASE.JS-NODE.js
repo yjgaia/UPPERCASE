baf7ec88576cfdad8f53220469502ad92d32c87d
hanul <hanul@hanul.me>
2015-10-12 03:02:24 +0900

    add SHA256, SHA512

--- UPPERCASE.JS-NODE.js
+++ UPPERCASE.JS-NODE.js
@@ -2251,6 +2251,56 @@ global.SHA1 = METHOD({
 	}
 });
 
+/**
+ * HMAC SHA256 encrypt.
+ */
+global.SHA256 = METHOD({
+
+	run : function(params) {
+		'use strict';
+		//REQUIRED: params
+		//REQUIRED: params.password
+		//REQUIRED: params.key
+
+		var
+		// password
+		password = params.password,
+
+		// key
+		key = params.key,
+
+		// crypto
+		crypto = require('crypto');
+
+		return crypto.createHmac('sha256', key).update(password).digest('hex');
+	}
+});
+
+/**
+ * HMAC SHA512 encrypt.
+ */
+global.SHA512 = METHOD({
+
+	run : function(params) {
+		'use strict';
+		//REQUIRED: params
+		//REQUIRED: params.password
+		//REQUIRED: params.key
+
+		var
+		// password
+		password = params.password,
+
+		// key
+		key = params.key,
+
+		// crypto
+		crypto = require('crypto');
+
+		return crypto.createHmac('sha512', key).update(password).digest('hex');
+	}
+});
+
 /*
  * check is exists file.
  */
