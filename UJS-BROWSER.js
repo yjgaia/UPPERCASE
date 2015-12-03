be6ee53699b9020eb3ade1bcf425df028fa5b1a0
Hanul <hanul@hanul.me>
2015-12-03 21:27:12 +0900

    update UJS

--- UJS-BROWSER.js
+++ UJS-BROWSER.js
@@ -7357,16 +7357,49 @@ FOR_BOX(function(box) {
 /**
  * go another view.
  */
-global.GO = METHOD({
+global.GO = METHOD(function(m) {
+	'use strict';
+	
+	var
+	// is ctrl key down
+	isCTRLKeyDown;
 
-	run : function(uri) {
-		'use strict';
-		//REQUIRED: uri
-
-		history.pushState(undefined, undefined, HREF(uri));
+	return {
 		
-		MATCH_VIEW.checkAll();
-	}
+		run : function(uri) {
+			//REQUIRED: uri
+			
+			if (isCTRLKeyDown === undefined) {
+				isCTRLKeyDown = false;
+							
+				EVENT('keydown', function(e) {
+					if (e.getKeyCode() === 17) {
+						isCTRLKeyDown = true;
+					}
+				});
+				
+				EVENT('keyup', function(e) {
+					if (e.getKeyCode() === 17) {
+						isCTRLKeyDown = false;
+					}
+				});
+			}
+			
+			if (isCTRLKeyDown === true) {
+				
+				GO_NEW_WIN(uri);
+				
+				isCTRLKeyDown = false;
+			}
+			
+			else {
+				
+				history.pushState(undefined, undefined, HREF(uri));
+				
+				MATCH_VIEW.checkAll();
+			}
+		}
+	};
 });
 
 FOR_BOX(function(box) {
