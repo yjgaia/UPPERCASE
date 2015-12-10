6a51963dcdf9cd8fb379d44e205a5036cc2ba1f2
Hanul <hanul@hanul.me>
2015-12-11 01:48:21 +0900

    update UJS

--- UJS-BROWSER.js
+++ UJS-BROWSER.js
@@ -3452,59 +3452,77 @@ global.LOAD = METHOD({
 /**
  * get/set meta description.
  */
-global.META_DESCRIPTION = METHOD({
-
-	run : function(description) {
-		'use strict';
-		
-		var
-		// meta tags
-		metaTags,
-		
-		// i
-		i,
-		
-		// is set
-		isSet = false,
-		
-		// new meta tag
-		newMetaTag;
-		
-		// get.
-		if (description === undefined) {
-			description = '';
+global.META_DESCRIPTION = METHOD(function() {
+	
+	var
+	// map
+	map = {
+		'&' : '&amp;',
+		'<' : '&lt;',
+		'>' : '&gt;',
+		'"' : '&quot;',
+		'\'' : '&#039;'
+	};
+	
+	return {
+	
+		run : function(description) {
+			'use strict';
 			
-			metaTags = document.getElementsByTagName('meta');
+			var
+			// meta tags
+			metaTags,
 			
-			for (i = 0; i < metaTags.length; i += 1) {
-				if (metaTags[i].name.toLowerCase() === 'description') {
-					description = metaTags[i].content;
-				}
-			}
-		}
-		
-		// set.
-		else {
-			metaTags = document.getElementsByTagName('meta');
+			// i
+			i,
 			
-			for (i = 0; i < metaTags.length; i += 1) {
-				if (metaTags[i].name.toLowerCase() === 'description') {
-					metaTags[i].content = description;
-					isSet = true;
-				}
-			}
+			// is set
+			isSet = false,
 			
-			if (isSet !== true) {
-				newMetaTag = document.createElement('meta');
-				newMetaTag.name = 'description';
-				newMetaTag.content = description;
+			// new meta tag
+			newMetaTag;
+			
+			// get.
+			if (description === undefined) {
+				description = '';
 				
-				document.getElementsByTagName('head')[0].appendChild(newMetaTag);
+				metaTags = document.getElementsByTagName('meta');
+				
+				for (i = 0; i < metaTags.length; i += 1) {
+					if (metaTags[i].name.toLowerCase() === 'description') {
+						description = metaTags[i].content;
+					}
+				}
 			}
+			
+			// set.
+			else {
+				
+				description = description.replace(/[&<>"']/g, function(c) {
+					return map[c];
+				});
+				
+				metaTags = document.getElementsByTagName('meta');
+				
+				for (i = 0; i < metaTags.length; i += 1) {
+					if (metaTags[i].name.toLowerCase() === 'description') {
+						metaTags[i].content = description;
+						isSet = true;
+					}
+				}
+				
+				if (isSet !== true) {
+					newMetaTag = document.createElement('meta');
+					newMetaTag.name = 'description';
+					newMetaTag.content = description;
+					
+					document.getElementsByTagName('head')[0].appendChild(newMetaTag);
+				}
+			}
+	
+			return description;
 		}
-
-		return description;
-	}
+	};
 });
 
 /**
