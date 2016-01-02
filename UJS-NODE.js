1ade8d933fee5d3472b3b493f1bd2205aaebe33f
Hanul <hanul@hanul.me>
2016-01-02 17:18:36 +0900

    UJS 업데이트

--- UJS-NODE.js
+++ UJS-NODE.js
@@ -8615,3 +8615,74 @@ global.CREATE_COOKIE_STR_ARRAY = CREATE_COOKIE_STR_ARRAY = METHOD({
 		return strs;
 	}
 });
+
+/**
+ * get CPU_USAGES.
+ */
+global.CPU_USAGES = METHOD(function(m) {
+	
+	var
+	//IMPORT: os
+	os = require('os');
+	
+	return {
+		
+		run : function() {
+			'use strict';
+			
+			var
+			// cpu infos
+			cpuInfos = os.cpus(),
+			
+			// usages
+			usages = [];
+			
+			EACH(cpuInfos, function(cpuInfo) {
+				
+				var
+				// total
+				total = 0,
+				
+				// idle time
+				idleTime;
+				
+				EACH(cpuInfo.times, function(time, type) {
+					total += time;
+					if (type === 'idle') {
+						idleTime = time;
+					}
+				});
+				
+				usages.push((1 - idleTime / total) * 100);
+			});
+			
+			return usages;
+		}
+	};
+});
+
+/**
+ * get MEMORY_USAGE.
+ */
+global.MEMORY_USAGE = METHOD(function(m) {
+	
+	var
+	//IMPORT: os
+	os = require('os'),
+	
+	// total memory
+	totalMemory = os.totalmem();
+	
+	return {
+		
+		run : function() {
+			'use strict';
+			
+			var
+			// free memory
+			freeMemory = os.freemem();
+			
+			return (1 - freeMemory / totalMemory) * 100;
+		}
+	};
+});
