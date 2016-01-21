c842015838e009a8fc64f551a1da790ffd586074
Hanul <hanul@hanul.me>
2016-01-21 17:11:48 +0900

    bug fix

--- UPPERCASE-DB/NODE.js
+++ UPPERCASE-DB/NODE.js
@@ -97,76 +97,8 @@ FOR_BOX(function(box) {
 	box.DB = CLASS(function(cls) {
 		
 		var
-		// generate _id.
-		gen_id,
-		
-		// make up filter.
-		makeUpFilter,
-		
 		// remove empty values.
 		removeEmptyValues;
-		
-		cls.gen_id = gen_id = function(id) {
-			//REQUIRED: id
-			
-			if (isNotUsingObjectId === true) {
-				return id;
-			} else {
-				return VALID.id(id) === true ? new ObjectID(id) : -1;
-			}
-		};
-		
-		cls.makeUpFilter = makeUpFilter = function(filter) {
-
-			var
-			// f.
-			f = function(filter) {
-
-				if (filter.id !== undefined) {
-
-					if (CHECK_IS_DATA(filter.id) === true) {
-
-						EACH(filter.id, function(values, i) {
-							if (CHECK_IS_DATA(values) === true || CHECK_IS_ARRAY(values) === true) {
-								EACH(values, function(value, j) {
-									values[j] = gen_id(value);
-								});
-							} else {
-								filter.id[i] = gen_id(values);
-							}
-						});
-
-						filter._id = filter.id;
-
-					} else {
-						filter._id = gen_id(filter.id);
-					}
-					delete filter.id;
-				}
-
-				EACH(filter, function(value, name) {
-					if (value === undefined) {
-						delete filter[name];
-					}
-				});
-			};
-
-			if (filter.$and !== undefined) {
-
-				EACH(filter.$and, function(filter) {
-					f(filter);
-				});
-
-			} else if (filter.$or !== undefined) {
-
-				EACH(filter.$or, function(filter) {
-					f(filter);
-				});
-
-			} else {
-				f(filter);
-			}
-		};
 		
 		cls.removeEmptyValues = removeEmptyValues = function(data) {
 			//REQUIRED: data
@@ -238,6 +170,17 @@ FOR_BOX(function(box) {
 				// waiting find all indexes infos
 				waitingFindAllIndexesInfos = [],
 	
+				// generate _id.
+				gen_id = function(id) {
+					//REQUIRED: id
+					
+					if (isNotUsingObjectId === true) {
+						return id;
+					} else {
+						return VALID.id(id) === true ? new ObjectID(id) : -1;
+					}
+				},
+	
 				// clean data.
 				cleanData = function(data) {
 					//REQUIRED: data
@@ -252,6 +195,59 @@ FOR_BOX(function(box) {
 	
 					// delete __RANDOM_KEY.
 					delete data.__RANDOM_KEY;
+				},
+	
+				// make up filter.
+				makeUpFilter = function(filter) {
+	
+					var
+					// f.
+					f = function(filter) {
+	
+						if (filter.id !== undefined) {
+	
+							if (CHECK_IS_DATA(filter.id) === true) {
+	
+								EACH(filter.id, function(values, i) {
+									if (CHECK_IS_DATA(values) === true || CHECK_IS_ARRAY(values) === true) {
+										EACH(values, function(value, j) {
+											values[j] = gen_id(value);
+										});
+									} else {
+										filter.id[i] = gen_id(values);
+									}
+								});
+	
+								filter._id = filter.id;
+	
+							} else {
+								filter._id = gen_id(filter.id);
+							}
+							delete filter.id;
+						}
+	
+						EACH(filter, function(value, name) {
+							if (value === undefined) {
+								delete filter[name];
+							}
+						});
+					};
+	
+					if (filter.$and !== undefined) {
+	
+						EACH(filter.$and, function(filter) {
+							f(filter);
+						});
+	
+					} else if (filter.$or !== undefined) {
+	
+						EACH(filter.$or, function(filter) {
+							f(filter);
+						});
+	
+					} else {
+						f(filter);
+					}
 				},
 	
 				// clean filter.
@@ -2486,62 +2482,91 @@ FOR_BOX(function(box) {
 	/**
 	 * MongoDB collection wrapper class for logging
 	 */
-	box.LOG_DB = CLASS({
-
-		init : function(inner, self, name) {
-			//REQUIRED: name
+	box.LOG_DB = CLASS(function(cls) {
+		
+		var
+		// generate _id.
+		gen_id = function(id) {
+			//REQUIRED: id
+			
+			return VALID.id(id) === true ? new ObjectID(id) : -1;
+		},
+		
+		// make up filter.
+		makeUpFilter = function(filter) {
 
 			var
-			// waiting log data set
-			waitingLogDataSet = [],
-			
-			// waiting find infos
-			waitingFindInfos = [],
+			// f.
+			f = function(filter) {
 
-			// log.
-			log,
-			
-			// find.
-			find;
+				if (filter.id !== undefined) {
 
-			self.log = log = function(data) {
-				//REQUIRED: data
+					if (CHECK_IS_DATA(filter.id) === true) {
 
-				waitingLogDataSet.push(data);
-			};
-			
-			self.find = find = function(params, callbackOrHandlers) {
-				//OPTIONAL: params
-				//OPTIONAL: params.filter
-				//OPTIONAL: params.sort
-				//OPTIONAL: params.start
-				//OPTIONAL: params.count
-				//OPTIONAL: params.isFindAll
-				//REQUIRED: callbackOrHandlers
-				//REQUIRED: callbackOrHandlers.success
-				//OPTIONAL: callbackOrHandlers.error
+						EACH(filter.id, function(values, i) {
+							if (CHECK_IS_DATA(values) === true || CHECK_IS_ARRAY(values) === true) {
+								EACH(values, function(value, j) {
+									values[j] = gen_id(value);
+								});
+							} else {
+								filter.id[i] = gen_id(values);
+							}
+						});
 
-				waitingFindInfos.push({
-					params : params,
-					callbackOrHandlers : callbackOrHandlers
+						filter._id = filter.id;
+
+					} else {
+						filter._id = gen_id(filter.id);
+					}
+					delete filter.id;
+				}
+
+				EACH(filter, function(value, name) {
+					if (value === undefined) {
+						delete filter[name];
+					}
 				});
 			};
 
-			CONNECT_TO_DB_SERVER.addInitDBFunc(function(nativeDB) {
+			if (filter.$and !== undefined) {
 
+				EACH(filter.$and, function(filter) {
+					f(filter);
+				});
+
+			} else if (filter.$or !== undefined) {
+
+				EACH(filter.$or, function(filter) {
+					f(filter);
+				});
+
+			} else {
+				f(filter);
+			}
+		};
+
+		return {
+		
+			init : function(inner, self, name) {
+				//REQUIRED: name
+	
 				var
-				// MongoDB collection
-				collection = nativeDB.collection(box.boxName + '.' + name);
-
+				// waiting log data set
+				waitingLogDataSet = [],
+				
+				// waiting find infos
+				waitingFindInfos = [],
+	
+				// log.
+				log,
+				
+				// find.
+				find;
+	
 				self.log = log = function(data) {
 					//REQUIRED: data
-
-					// now
-					data.time = new Date();
-					
-					box.DB.removeEmptyValues(data);
-
-					collection.insertOne(data);
+	
+					waitingLogDataSet.push(data);
 				};
 				
 				self.find = find = function(params, callbackOrHandlers) {
@@ -2555,160 +2580,195 @@ FOR_BOX(function(box) {
 					//REQUIRED: callbackOrHandlers.success
 					//OPTIONAL: callbackOrHandlers.error
 	
+					waitingFindInfos.push({
+						params : params,
+						callbackOrHandlers : callbackOrHandlers
+					});
+				};
+	
+				CONNECT_TO_DB_SERVER.addInitDBFunc(function(nativeDB) {
+	
 					var
-					// filter
-					filter,
-
-					// sort
-					sort,
-
-					// start
-					start,
-
-					// count
-					count,
-
-					// is find all
-					isFindAll,
-					
-					// callback
-					callback,
-
-					// error handler
-					errorHandler,
-
-					// error message
-					errorMsg,
-					
-					// cleaned filter
-					cleanedFilter,
-					
-					// cached info
-					cachedInfo,
-
-					// proc.
-					proc;
-
-					try {
-
-						if (callbackOrHandlers === undefined) {
-							callbackOrHandlers = params;
-							params = undefined;
-						}
-
-						if (params !== undefined) {
-							filter = params.filter;
-							sort = params.sort;
-							start = INTEGER(params.start);
-							count = INTEGER(params.count);
-							isFindAll = params.isFindAll;
-						}
-
-						if (CHECK_IS_DATA(callbackOrHandlers) !== true) {
-							callback = callbackOrHandlers;
-						} else {
-							callback = callbackOrHandlers.success;
-							errorHandler = callbackOrHandlers.error;
-						}
-
-						if (filter === undefined) {
-							filter = {};
-						}
-
-						if (sort === undefined) {
-							sort = {
-								createTime : -1
-							};
-						} 
-					
-						else if (sort.id !== undefined) {
-							sort._id = sort.id;
-							delete sort.id;
-						}
+					// MongoDB collection
+					collection = nativeDB.collection(box.boxName + '.' + name);
+	
+					self.log = log = function(data) {
+						//REQUIRED: data
+	
+						// now
+						data.time = new Date();
 						
-						if (sort.createTime === undefined) {
-							sort.createTime = -1;
-						}
-
-						if (start === undefined || start < 0) {
-							start = 0;
-						}
-
-						if (isFindAll !== true) {
-							if (count === undefined || count > NODE_CONFIG.maxDataCount || isNaN(count) === true) {
-								count = NODE_CONFIG.maxDataCount;
-							} else if (count < 1) {
-								count = 1;
-							}
-						}
-
-						box.DB.makeUpFilter(filter);
-
-						proc = function(error, savedDataSet) {
-
-							if (error === TO_DELETE) {
-								
-								// clean saved data before callback.
-								EACH(savedDataSet, function(savedData, i) {
-									
-									// convert _id (object) to id (string).
-									if (savedData._id !== undefined) {
-										savedData.id = savedData._id.toString();
-									}
+						box.DB.removeEmptyValues(data);
+	
+						collection.insertOne(data);
+					};
 					
-									// delete _id.
-									delete savedData._id;
-								});
-
-								callback(savedDataSet);
+					self.find = find = function(params, callbackOrHandlers) {
+						//OPTIONAL: params
+						//OPTIONAL: params.filter
+						//OPTIONAL: params.sort
+						//OPTIONAL: params.start
+						//OPTIONAL: params.count
+						//OPTIONAL: params.isFindAll
+						//REQUIRED: callbackOrHandlers
+						//REQUIRED: callbackOrHandlers.success
+						//OPTIONAL: callbackOrHandlers.error
+		
+						var
+						// filter
+						filter,
+	
+						// sort
+						sort,
+	
+						// start
+						start,
+	
+						// count
+						count,
+	
+						// is find all
+						isFindAll,
+						
+						// callback
+						callback,
+	
+						// error handler
+						errorHandler,
+	
+						// error message
+						errorMsg,
+						
+						// cleaned filter
+						cleanedFilter,
+						
+						// cached info
+						cachedInfo,
+	
+						// proc.
+						proc;
+	
+						try {
+	
+							if (callbackOrHandlers === undefined) {
+								callbackOrHandlers = params;
+								params = undefined;
 							}
-
-							// if error is not TO_DELETE
-							else {
-
-								if (errorHandler !== undefined) {
-									errorHandler(error.toString());
-								} else {
-									console.log(CONSOLE_RED('[UPPERCASE-DB] `' + box.boxName + '.' + name + '` LOG_DB ERROR:'), error.toString());
+	
+							if (params !== undefined) {
+								filter = params.filter;
+								sort = params.sort;
+								start = INTEGER(params.start);
+								count = INTEGER(params.count);
+								isFindAll = params.isFindAll;
+							}
+	
+							if (CHECK_IS_DATA(callbackOrHandlers) !== true) {
+								callback = callbackOrHandlers;
+							} else {
+								callback = callbackOrHandlers.success;
+								errorHandler = callbackOrHandlers.error;
+							}
+	
+							if (filter === undefined) {
+								filter = {};
+							}
+	
+							if (sort === undefined) {
+								sort = {
+									createTime : -1
+								};
+							} 
+						
+							else if (sort.id !== undefined) {
+								sort._id = sort.id;
+								delete sort.id;
+							}
+							
+							if (sort.createTime === undefined) {
+								sort.createTime = -1;
+							}
+	
+							if (start === undefined || start < 0) {
+								start = 0;
+							}
+	
+							if (isFindAll !== true) {
+								if (count === undefined || count > NODE_CONFIG.maxDataCount || isNaN(count) === true) {
+									count = NODE_CONFIG.maxDataCount;
+								} else if (count < 1) {
+									count = 1;
 								}
 							}
-						};
+	
+							makeUpFilter(filter);
+	
+							proc = function(error, savedDataSet) {
+	
+								if (error === TO_DELETE) {
+									
+									// clean saved data before callback.
+									EACH(savedDataSet, function(savedData, i) {
+										
+										// convert _id (object) to id (string).
+										if (savedData._id !== undefined) {
+											savedData.id = savedData._id.toString();
+										}
 						
-						if (isFindAll === true) {
-
-							// find all data set.
-							collection.find(filter).sort(sort).skip(start).toArray(proc);
-
-						} else {
-
-							collection.find(filter).sort(sort).skip(start).limit(count).toArray(proc);
+										// delete _id.
+										delete savedData._id;
+									});
+	
+									callback(savedDataSet);
+								}
+	
+								// if error is not TO_DELETE
+								else {
+	
+									if (errorHandler !== undefined) {
+										errorHandler(error.toString());
+									} else {
+										console.log(CONSOLE_RED('[UPPERCASE-DB] `' + box.boxName + '.' + name + '` LOG_DB ERROR:'), error.toString());
+									}
+								}
+							};
+							
+							if (isFindAll === true) {
+	
+								// find all data set.
+								collection.find(filter).sort(sort).skip(start).toArray(proc);
+	
+							} else {
+	
+								collection.find(filter).sort(sort).skip(start).limit(count).toArray(proc);
+							}
 						}
-					}
-
-					// if catch error
-					catch (error) {
-						
-						if (errorHandler !== undefined) {
-							errorHandler(error.toString());
-						} else {
-							console.log(CONSOLE_RED('[UPPERCASE-DB] `' + box.boxName + '.' + name + '` LOG_DB ERROR:'), error.toString());
+	
+						// if catch error
+						catch (error) {
+							
+							if (errorHandler !== undefined) {
+								errorHandler(error.toString());
+							} else {
+								console.log(CONSOLE_RED('[UPPERCASE-DB] `' + box.boxName + '.' + name + '` LOG_DB ERROR:'), error.toString());
+							}
 						}
-					}
-				};
-
-				EACH(waitingLogDataSet, function(data) {
-					log(data);
+					};
+	
+					EACH(waitingLogDataSet, function(data) {
+						log(data);
+					});
+	
+					waitingLogDataSet = undefined;
+					
+					EACH(waitingFindInfos, function(info) {
+						find(info.params, info.callbackOrHandlers);
+					});
+	
+					waitingFindInfos = undefined;
 				});
-
-				waitingLogDataSet = undefined;
-				
-				EACH(waitingFindInfos, function(info) {
-					find(info.params, info.callbackOrHandlers);
-				});
-
-				waitingFindInfos = undefined;
-			});
-		}
+			}
+		};
 	});
 });
 
