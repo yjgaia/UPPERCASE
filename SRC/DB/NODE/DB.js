026a4cec0c45ab7924d94a02931b83ff2b42f975
Hanul <hanul@hanul.me>
2016-05-07 17:22:00 +0900

    update history 버그 개선

--- SRC/DB/NODE/DB.js
+++ SRC/DB/NODE/DB.js
@@ -1473,7 +1473,7 @@ FOR_BOX(function(box) {
 															isSame = true;
 															
 															EACH(updateData, function(value, name) {
-																if (name !== 'lastUpdateTime' && savedData[name] !== value) {
+																if (name !== 'lastUpdateTime' && originData[name] !== value) {
 																	isSame = false;
 																	return false;
 																}
@@ -1481,7 +1481,7 @@ FOR_BOX(function(box) {
 															
 															return isSame;
 															
-														}) === true) {
+														}) !== true) {
 															addHistory('update', id, updateData, savedData.lastUpdateTime);
 														}
 				
--- UPPERCASE-DB/NODE.js
+++ UPPERCASE-DB/NODE.js
@@ -1556,7 +1556,7 @@ FOR_BOX(function(box) {
 															isSame = true;
 															
 															EACH(updateData, function(value, name) {
-																if (name !== 'lastUpdateTime' && savedData[name] !== value) {
+																if (name !== 'lastUpdateTime' && originData[name] !== value) {
 																	isSame = false;
 																	return false;
 																}
@@ -1564,7 +1564,7 @@ FOR_BOX(function(box) {
 															
 															return isSame;
 															
-														}) === true) {
+														}) !== true) {
 															addHistory('update', id, updateData, savedData.lastUpdateTime);
 														}
 				
