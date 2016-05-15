148889c042af7bc594a6e941e31a48a423bb1bec
Hanul <hanul@hanul.me>
2016-05-15 21:11:35 +0900

    bug fix

--- UPPERCASE-BROWSER-PACK/IMPORT.js
+++ UPPERCASE-BROWSER-PACK/IMPORT.js
@@ -733,59 +733,10 @@ global.PARALLEL = METHOD({
 		// done count
 		doneCount = 0;
 
+		// only funcs
 		if (funcs === undefined) {
 			funcs = countOrArray;
-			countOrArray = undefined;
-		}
-
-		if (countOrArray !== undefined) {
-			if (CHECK_IS_ARRAY(countOrArray) !== true) {
-				count = countOrArray;
-			} else {
-				array = countOrArray;
-			}
-		}
-
-		if (count !== undefined) {
-
-			if (count === 0) {
-				funcs[1]();
-			} else {
-
-				REPEAT(count, function(i) {
-
-					funcs[0](i, function() {
-
-						doneCount += 1;
-
-						if (doneCount === count) {
-							funcs[1]();
-						}
-					});
-				});
-			}
-
-		} else if (array !== undefined) {
-
-			if (array.length === 0) {
-				funcs[1]();
-			} else {
-
-				EACH(array, function(value, i) {
-
-					funcs[0](value, function() {
-
-						doneCount += 1;
-
-						if (doneCount === array.length) {
-							funcs[1]();
-						}
-					}, i);
-				});
-			}
-
-		} else {
-
+			
 			RUN(function() {
 
 				var
@@ -807,6 +758,61 @@ global.PARALLEL = METHOD({
 					}
 				});
 			});
+		}
+		
+		else {
+			
+			if (countOrArray === undefined) {
+				funcs[1]();
+			}
+			
+			else {
+				
+				if (CHECK_IS_ARRAY(countOrArray) !== true) {
+					count = countOrArray;
+				} else {
+					array = countOrArray;
+				}
+		
+				if (count !== undefined) {
+		
+					if (count === 0) {
+						funcs[1]();
+					} else {
+		
+						REPEAT(count, function(i) {
+		
+							funcs[0](i, function() {
+		
+								doneCount += 1;
+		
+								if (doneCount === count) {
+									funcs[1]();
+								}
+							});
+						});
+					}
+		
+				} else if (array !== undefined) {
+		
+					if (array.length === 0) {
+						funcs[1]();
+					} else {
+		
+						EACH(array, function(value, i) {
+		
+							funcs[0](value, function() {
+		
+								doneCount += 1;
+		
+								if (doneCount === array.length) {
+									funcs[1]();
+								}
+							}, i);
+						});
+					}
+				}
+			}
 		}
 	}
 });
