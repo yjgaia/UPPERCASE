278496277d9204e1cd1c18faa668d1d708f18da3
Hanul <hanul@hanul.me>
2016-05-16 00:33:41 +0900

    bug fix

--- UPPERCASE-TRANSPORT/NODE.js
+++ UPPERCASE-TRANSPORT/NODE.js
@@ -685,8 +685,11 @@ global.WEB_SOCKET_SERVER = METHOD({
 		//REQUIRED: connectionListener
 
 		var
+		//IMPORT: WebSocket
+		WebSocket = require('ws'),
+		
 		//IMPORT: WebSocketServer
-		WebSocketServer = require('ws').Server,
+		WebSocketServer = WebSocket.Server,
 		
 		// native connection listener.
 		nativeConnectionListener = function(conn) {
@@ -857,36 +860,39 @@ global.WEB_SOCKET_SERVER = METHOD({
 				// callback name
 				callbackName;
 				
-				try {
+				if (conn.readyState === WebSocket.OPEN) {
 					
-					conn.send(STRINGIFY({
-						methodName : params.methodName,
-						data : params.data,
-						sendKey : sendKey
-					}));
+					try {
+						
+						conn.send(STRINGIFY({
+							methodName : params.methodName,
+							data : params.data,
+							sendKey : sendKey
+						}));
+						
+					} catch(error) {
+						console.log(CONSOLE_RED('[UPPERCASE-WEB_SOCEKT_SERVER] ERROR:'), error.toString());
+					}
+	
+					if (callback !== undefined) {
+						
+						callbackName = '__CALLBACK_' + sendKey;
+	
+						// on callback.
+						on(callbackName, function(data) {
+	
+							// run callback.
+							callback(data);
+	
+							// off callback.
+							off(callbackName);
+						});
+					}
+	
+					sendKey += 1;
 					
-				} catch(error) {
-					console.log(CONSOLE_RED('[UPPERCASE-WEB_SOCEKT_SERVER] ERROR:'), error.toString());
+					clientInfo.lastReceiveTime = new Date();
 				}
-
-				if (callback !== undefined) {
-					
-					callbackName = '__CALLBACK_' + sendKey;
-
-					// on callback.
-					on(callbackName, function(data) {
-
-						// run callback.
-						callback(data);
-
-						// off callback.
-						off(callbackName);
-					});
-				}
-
-				sendKey += 1;
-				
-				clientInfo.lastReceiveTime = new Date();
 			},
 
 			// disconnect.
