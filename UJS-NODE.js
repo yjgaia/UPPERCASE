7d6245ae6c70912d2cb325d9490a05cc4d10149f
Hanul <hanul@hanul.me>
2016-05-20 01:44:24 +0900

    update UJS-NODE

--- UJS-NODE.js
+++ UJS-NODE.js
@@ -8704,7 +8704,7 @@ global.SOCKET_SERVER = METHOD({
 				// error msg
 				errorMsg;
 				
-				if (error.code !== 'ECONNRESET' && error.code !== 'EPIPE' && error.code !== 'ETIMEDOUT' && error.code !== 'ENETUNREACH' && error.code !== 'EHOSTUNREACH' && error.code !== 'ECONNREFUSED') {
+				if (error.code !== 'ECONNRESET' && error.code !== 'EPIPE' && error.code !== 'ETIMEDOUT' && error.code !== 'ENETUNREACH' && error.code !== 'EHOSTUNREACH' && error.code !== 'ECONNREFUSED' && error.code !== 'EINVAL') {
 					
 					errorMsg = error.toString();
 					
