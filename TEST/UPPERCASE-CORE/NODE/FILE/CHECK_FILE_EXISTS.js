20ede5c966d55e94369abeb6c77ff10beb994a56
YJ <hanul@hanul.me>
2017-08-25 03:45:40 +0900

    MOVE_FILE 경로 동일할 경우 처리

--- TEST/UPPERCASE-CORE/NODE/FILE/CHECK_FILE_EXISTS.js
+++ TEST/UPPERCASE-CORE/NODE/FILE/CHECK_FILE_EXISTS.js
@@ -1,6 +1,6 @@
-TEST('CHECK_IS_FILE_EXISTS', (check) => {
+TEST('CHECK_FILE_EXISTS', (check) => {
 
-	check(CHECK_IS_FILE_EXISTS({
+	check(CHECK_FILE_EXISTS({
 		path : 'UPPERCASE-CORE/test.txt',
 		isSync : true
 	}) === true);
--- UPPERCASE-CORE/NODE.js
+++ UPPERCASE-CORE/NODE.js
@@ -5595,41 +5595,53 @@ global.MOVE_FILE = METHOD({
 		//REQUIRED: params.from		파일의 원래 위치
 		//REQUIRED: params.to		파일을 옮길 위치
 		//OPTIONAL: params.isSync	true로 설정하면 callback을 실행하지 않고 즉시 실행합니다. 이 설정은 명령이 끝날때 까지 프로그램이 멈추게 되므로 필요한 경우에만 사용합니다.
-		//REQUIRED: callbackOrHandlers
+		//OPTIONAL: callbackOrHandlers
 		//OPTIONAL: callbackOrHandlers.notExistsHandler
 		//OPTIONAL: callbackOrHandlers.error
-		//REQUIRED: callbackOrHandlers.success
+		//OPTIONAL: callbackOrHandlers.success
 
 		let from = params.from;
+		let to = params.to;
 		let isSync = params.isSync;
 		
 		let notExistsHandler;
 		let errorHandler;
 		let callback;
 
-		if (CHECK_IS_DATA(callbackOrHandlers) !== true) {
-			callback = callbackOrHandlers;
-		} else {
-			notExistsHandler = callbackOrHandlers.notExists;
-			errorHandler = callbackOrHandlers.error;
-			callback = callbackOrHandlers.success;
-		}
-
-		COPY_FILE(params, {
-			error : errorHandler,
-			notExists : notExistsHandler,
-			success : () => {
-
-				REMOVE_FILE({
-					path : from,
-					isSync : isSync
-				}, {
-					error : errorHandler,
-					notExists : notExistsHandler,
-					success : callback
-				});
+		if (callbackOrHandlers !== undefined) {
+			if (CHECK_IS_DATA(callbackOrHandlers) !== true) {
+				callback = callbackOrHandlers;
+			} else {
+				notExistsHandler = callbackOrHandlers.notExists;
+				errorHandler = callbackOrHandlers.error;
+				callback = callbackOrHandlers.success;
 			}
-		});
+		}
+		
+		if (from === to) {
+			if (callback !== undefined) {
+				callback();
+			}
+		}
+		
+		else {
+			
+			COPY_FILE(params, {
+				error : errorHandler,
+				notExists : notExistsHandler,
+				success : () => {
+	
+					REMOVE_FILE({
+						path : from,
+						isSync : isSync
+					}, {
+						error : errorHandler,
+						notExists : notExistsHandler,
+						success : callback
+					});
+				}
+			});
+		}
 	}
 });
 
