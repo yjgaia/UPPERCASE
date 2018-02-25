50bc735388cbe17f8c35c2bfb143736fcce1d78b
YJ <hanul@hanul.me>
2018-02-25 15:47:58 +0900

    no message

--- UPPERCASE-CORE/BROWSER.js
+++ UPPERCASE-CORE/BROWSER.js
@@ -9921,12 +9921,12 @@ global.REQUEST = METHOD({
 		
 		(method === 'GET' || method === 'DELETE' ? fetch(url + '?' + paramStr, {
 			method : method,
-			credentials : host === BROWSER_CONFIG.host && port === BROWSER_CONFIG.port ? 'include' : undefined,
+			credentials : location.protocol !== 'file:' && host === BROWSER_CONFIG.host && port === BROWSER_CONFIG.port ? 'include' : undefined,
 			headers : new Headers(headers === undefined ? {} : headers)
 		}) : fetch(url, {
 			method : method,
 			body : paramStr,
-			credentials : host === BROWSER_CONFIG.host && port === BROWSER_CONFIG.port ? 'include' : undefined,
+			credentials : location.protocol !== 'file:' && host === BROWSER_CONFIG.host && port === BROWSER_CONFIG.port ? 'include' : undefined,
 			headers : new Headers(headers === undefined ? {} : headers)
 		}))
 		.then((response) => {
