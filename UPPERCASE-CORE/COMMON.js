e967be57ae94ab059dd1b9d252c9bb0205cb6c5e
Hanul <hanul@hanul.me>
2018-06-10 21:53:09 +0900

    버그 수정

--- UPPERCASE-CORE/COMMON.js
+++ UPPERCASE-CORE/COMMON.js
@@ -3964,7 +3964,26 @@ global.PARSE_STR = METHOD({
 				let array = [];
 				
 				EACH(data, (data) => {
-					array.push(UNPACK_DATA(data));
+					
+					if (CHECK_IS_DATA(data) === true) {
+						array.push(UNPACK_DATA(data));
+					}
+					
+					else if (CHECK_IS_ARRAY(data) === true) {
+						
+						EACH(data, (v, i) => {
+		
+							if (CHECK_IS_DATA(v) === true) {
+								data[i] = UNPACK_DATA(v);
+							}
+						});
+						
+						array.push(data);
+					}
+					
+					else {
+						array.push(data);
+					}
 				});
 				
 				return array;
