215c881287de2ac41a03a7a177764a784a974513
YJ <hanul@hanul.me>
2018-06-29 22:11:31 +0900

    DISK_USAGE 업데이트

--- UPPERCASE-CORE/NODE.js
+++ UPPERCASE-CORE/NODE.js
@@ -12841,19 +12841,32 @@ global.DISK_USAGE = METHOD(() => {
 			
 			if (drive === undefined) {
 				if (process.platform === 'win32') {
-					drive = 'c:';
+					drive = 'C';
 				} else {
 					drive = '/';
 				}
 			}
 			
-			diskspace.check(drive, (err, total, free, status) => {
-				if (status === 'READY') {
-					callback((1 - free / total) * 100);
-				} else if (errorHandler !== undefined) {
-					errorHandler(status);
-				} else {
-					SHOW_ERROR('DISK_USAGE', status);
+			diskspace.check(drive, (error, result) => {
+				
+				if (error !== TO_DELETE) {
+					if (errorHandler !== undefined) {
+						errorHandler(error.toString());
+					} else {
+						SHOW_ERROR('DISK_USAGE', error.toString());
+					}
+				}
+				
+				else if (result.status === 'READY') {
+					callback((1 - result.free / result.total) * 100);
+				}
+				
+				else {
+					if (errorHandler !== undefined) {
+						errorHandler(status);
+					} else {
+						SHOW_ERROR('DISK_USAGE', status);
+					}
 				}
 			});
 		}
