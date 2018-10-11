9bb1141fae29a2b3a9c07483def56208e9fa1946
YJ <hanul@hanul.me>
2018-10-11 22:13:58 +0900

    audiocontext-polyfill.js 개선

--- SRC/UPPERCASE-CORE/BROWSER/POLYFILL/audiocontext-polyfill.js
+++ SRC/UPPERCASE-CORE/BROWSER/POLYFILL/audiocontext-polyfill.js
@@ -1,105 +1,101 @@
 /*!
-audiocontext-polyfill.js v0.1.1
+audiocontext-polyfill.js v0.1.2
 (c) 2013 - 2014 Shinnosuke Watanabe
 Licensed under the MIT license
 */
+'use strict';
 
-(function(window, undefined) {
-  'use strict';
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-  window.AudioContext = window.AudioContext ||
-                        window.webkitAudioContext;
-  
-  if (window.AudioContext !== undefined) {
+if (window.AudioContext !== undefined) {
 
-    window.OfflineAudioContext = window.OfflineAudioContext ||
-                                 window.webkitOfflineAudioContext;
+	window.OfflineAudioContext = window.OfflineAudioContext || window.webkitOfflineAudioContext;
 
-    var Proto = AudioContext.prototype;
+	let Proto = AudioContext.prototype;
 
-    var tmpctx = new AudioContext();
+	let tmpctx = new AudioContext();
 
-    // Support alternate names
-    // start (noteOn), stop (noteOff), createGain (createGainNode), etc.
-    var isStillOld = function(normative, old) {
-      return normative === undefined && old !== undefined;
-    };
+	// Support alternate names
+	// start (noteOn), stop (noteOff), createGain (createGainNode), etc.
+	let isStillOld = (normative, old) => {
+		return normative === undefined && old !== undefined;
+	};
 
-    var bufProto = tmpctx.createBufferSource().constructor.prototype;
+	let bufProto = tmpctx.createBufferSource().constructor.prototype;
 
-    if (isStillOld(bufProto.start, bufProto.noteOn) ||
-    isStillOld(bufProto.stop, bufProto.noteOff)) {
-      var nativeCreateBufferSource = Proto.createBufferSource;
+	if (isStillOld(bufProto.start, bufProto.noteOn) || isStillOld(bufProto.stop, bufProto.noteOff)) {
+		
+		let nativeCreateBufferSource = Proto.createBufferSource;
 
-      Proto.createBufferSource = function createBufferSource() {
-        var returnNode = nativeCreateBufferSource.call(this);
-        returnNode.start = returnNode.start || returnNode.noteOn;
-        returnNode.stop = returnNode.stop || returnNode.noteOff;
+		Proto.createBufferSource = function() {
+			let returnNode = nativeCreateBufferSource.call(this);
+			returnNode.start = returnNode.start || returnNode.noteOn;
+			returnNode.stop = returnNode.stop || returnNode.noteOff;
 
-        return returnNode;
-      };
-    }
+			return returnNode;
+		};
+	}
 
-    // Firefox 24 doesn't support OscilatorNode
-    if (typeof tmpctx.createOscillator === 'function') {
-      var oscProto = tmpctx.createOscillator().constructor.prototype;
+	// Firefox 24 doesn't support OscilatorNode
+	if (typeof tmpctx.createOscillator === 'function') {
+		let oscProto = tmpctx.createOscillator().constructor.prototype;
 
-      if (isStillOld(oscProto.start, oscProto.noteOn) ||
-      isStillOld(oscProto.stop, oscProto.noteOff)) {
-        var nativeCreateOscillator = Proto.createOscillator;
+		if (isStillOld(oscProto.start, oscProto.noteOn) ||
+			isStillOld(oscProto.stop, oscProto.noteOff)) {
+			let nativeCreateOscillator = Proto.createOscillator;
 
-        Proto.createOscillator = function createOscillator() {
-          var returnNode = nativeCreateOscillator.call(this);
-          returnNode.start = returnNode.start || returnNode.noteOn;
-          returnNode.stop = returnNode.stop || returnNode.noteOff;
+			Proto.createOscillator = function() {
+				let returnNode = nativeCreateOscillator.call(this);
+				returnNode.start = returnNode.start || returnNode.noteOn;
+				returnNode.stop = returnNode.stop || returnNode.noteOff;
 
-          return returnNode;
-        };
-      }
-    }
+				return returnNode;
+			};
+		}
+	}
 
-    if (Proto.createGain === undefined && Proto.createGainNode !== undefined) {
-      Proto.createGain = Proto.createGainNode;
-    }
+	if (Proto.createGain === undefined && Proto.createGainNode !== undefined) {
+		Proto.createGain = Proto.createGainNode;
+	}
 
-    if (Proto.createDelay === undefined && Proto.createDelayNode !== undefined) {
-      Proto.createDelay = Proto.createGainNode;
-    }
+	if (Proto.createDelay === undefined && Proto.createDelayNode !== undefined) {
+		Proto.createDelay = Proto.createGainNode;
+	}
 
-    if (Proto.createScriptProcessor === undefined &&
-    Proto.createJavaScriptNode !== undefined) {
-      Proto.createScriptProcessor = Proto.createJavaScriptNode;
-    }
+	if (Proto.createScriptProcessor === undefined &&
+		Proto.createJavaScriptNode !== undefined) {
+		Proto.createScriptProcessor = Proto.createJavaScriptNode;
+	}
 
-    // Black magic for iOS
-    var is_iOS = (navigator.userAgent.indexOf('like Mac OS X') !== -1);
-    if (is_iOS) {
-      var OriginalAudioContext = AudioContext;
-      window.AudioContext = function AudioContext() {
-        var iOSCtx = new OriginalAudioContext();
+	// Black magic for iOS
+	if (navigator.userAgent.indexOf('like Mac OS X') !== -1) {
+		
+		let OriginalAudioContext = AudioContext;
+		
+		window.AudioContext = function() {
+			let iOSCtx = new OriginalAudioContext();
 
-        var body = document.body;
-        var tmpBuf = iOSCtx.createBufferSource();
-        var tmpProc = iOSCtx.createScriptProcessor(256, 1, 1);
+			let body = document.body;
+			let tmpBuf = iOSCtx.createBufferSource();
+			let tmpProc = iOSCtx.createScriptProcessor(256, 1, 1);
+			
+			let instantProcess = () => {
+				tmpBuf.start(0);
+				tmpBuf.connect(tmpProc);
+				tmpProc.connect(iOSCtx.destination);
+			};
 
-        body.addEventListener('touchstart', instantProcess, false);
+			body.addEventListener('touchstart', instantProcess, false);
 
-        function instantProcess() {
-          tmpBuf.start(0);
-          tmpBuf.connect(tmpProc);
-          tmpProc.connect(iOSCtx.destination);
-        }
+			// This function will be called once and for all.
+			tmpProc.onaudioprocess = () => {
+				tmpBuf.disconnect();
+				tmpProc.disconnect();
+				body.removeEventListener('touchstart', instantProcess, false);
+				tmpProc.onaudioprocess = null;
+			};
 
-        // This function will be called once and for all.
-        tmpProc.onaudioprocess = function() {
-          tmpBuf.disconnect();
-          tmpProc.disconnect();
-          body.removeEventListener('touchstart', instantProcess, false);
-          tmpProc.onaudioprocess = null;
-        };
-
-        return iOSCtx;
-      };
-    }
-  }
-}(window));
+			return iOSCtx;
+		};
+	}
+}
