04a4cf5df3f224fde493a045b96bc8f1e2cd33c6
YJ Sim <hanul@hanul.me>
2019-03-08 00:56:16 +0900

    MongoDB 도커 생성

--- DOCKER/mongodb/README.md
+++ DOCKER/mongodb/README.md
@@ -1,2 +1,3 @@
-MONGO_USERNAME
-MONGO_PASSWORD
+docker build . --tag hanul/mongodb:0.1
+
+docker run -v ~/test:/data/db -e MONGO_USERNAME=root -e MONGO_PASSWORD=test hanul/mongodb:0.1
--- DOCKER/mongodb/docker-entrypoint.sh
+++ DOCKER/mongodb/docker-entrypoint.sh
@@ -1,3 +1,3 @@
-RUN mongod --port 27018 --fork --logpath /var/log/mongodb.log --logappend
-RUN mongo --port 27018 --eval "use admin; db.createUser({ user : '${MONGO_USERNAME}', pwd : '${MONGO_PASSWORD}', roles : ['root'] }); db.shutdownServer();"
-RUN mongod --port 27018 --fork --logpath /var/log/mongodb.log --logappend --auth --bind_ip_all
+mongod --port 27018 --fork --logpath /var/log/mongodb.log --logappend
+mongo admin --port 27018 --eval "db.createUser({ user : '${MONGO_USERNAME}', pwd : '${MONGO_PASSWORD}', roles : ['root'] }); db.shutdownServer();"
+mongod --port 27018 --fork --logpath /var/log/mongodb.log --logappend --auth --bind_ip_all
