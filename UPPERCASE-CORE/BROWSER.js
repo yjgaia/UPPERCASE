4e9f002e6cdd19917755a5f9a4991b20e8b0abe5
Hanul <hanul@hanul.me>
2019-12-09 09:34:54 +0900

    여러 스타일을 한번에 지정할 수 있도록

--- UPPERCASE-CORE/BROWSER.js
+++ UPPERCASE-CORE/BROWSER.js
@@ -7748,62 +7748,77 @@ global.ADD_STYLE = METHOD({
 
 		let node = params.node;
 		let style = params.style;
-		let el = node.getWrapperEl();
-
-		EACH(style, (value, name) => {
+		
+		if (CHECK_IS_ARRAY(style) === true) {
 			
-			if (value !== undefined) {
+			EACH(style, (style) => {
+				
+				ADD_STYLE({
+					node : node,
+					style : style
+				});
+			});
+		}
+		
+		else {
+			
+			let el = node.getWrapperEl();
 
-				// on display resize
-				if (name === 'onDisplayResize') {
-
-					let resizeEvent = EVENT({
-						name : 'resize'
-					}, RAR(() => {
-
-						// when this, value is function.
-						ADD_STYLE({
-							node : node,
-							style : value(WIN_WIDTH(), WIN_HEIGHT())
-						});
-						
-						DELAY(() => {
+			EACH(style, (value, name) => {
+				
+				if (value !== undefined) {
+	
+					// on display resize
+					if (name === 'onDisplayResize') {
+	
+						let resizeEvent = EVENT({
+							name : 'resize'
+						}, RAR(() => {
+	
+							// when this, value is function.
 							ADD_STYLE({
 								node : node,
 								style : value(WIN_WIDTH(), WIN_HEIGHT())
 							});
+							
+							DELAY(() => {
+								ADD_STYLE({
+									node : node,
+									style : value(WIN_WIDTH(), WIN_HEIGHT())
+								});
+							});
+						}));
+	
+						// remove resize event when remove node.
+						node.on('remove', () => {
+							resizeEvent.remove();
 						});
-					}));
-
-					// remove resize event when remove node.
-					node.on('remove', () => {
-						resizeEvent.remove();
-					});
-
-				} else if (el !== undefined) {
-					
-					// flt -> float
-					if (name === 'flt') {
-						el.style.cssFloat = value;
-					}
-
-					// assume number value is px value.
-					else if (typeof value === 'number' && name !== 'zIndex' && name !== 'opacity') {
-						el.style[name] = value + 'px';
-					}
-					
-					// set background image. (not need url prefix.)
-					else if (name === 'backgroundImage' && value !== 'none') {
-						el.style[name] = 'url(' + value + ')';
-					}
-
-					// set normal style.
-					else {
-						el.style[name] = value;
+	
+					} else if (el !== undefined) {
+						
+						// flt -> float
+						if (name === 'flt') {
+							el.style.cssFloat = value;
+						}
+	
+						// assume number value is px value.
+						else if (typeof value === 'number' && name !== 'zIndex' && name !== 'opacity') {
+							el.style[name] = value + 'px';
+						}
+						
+						// set background image. (not need url prefix.)
+						else if (name === 'backgroundImage' && value !== 'none') {
+							el.style[name] = 'url(' + value + ')';
+						}
+	
+						// set normal style.
+						else {
+							el.style[name] = value;
+						}
 					}
 				}
-			}
-		});
+			});
+		}
 	}
 });
 
