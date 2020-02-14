12ad3594462ac3c202adce4deef202f313f5e969
Hanul <hanul@hanul.me>
2020-02-14 16:48:57 +0900

    npm 모듈 버그 수정

--- LOAD.js
+++ LOAD.js
@@ -1,11 +1,7 @@
 /*
  * UPPERCASE의 모든 모듈들을 불러옵니다.
  */
-require('./UPPERCASE-CORE/NODE.js');
-require('./UPPERCASE-ROOM/NODE.js');
-require('./UPPERCASE-DB/NODE.js');
-require('./UPPERCASE-MODEL/NODE.js');
-require('./UPPERCASE-BOOT/NODE.js');
+require('uppercase-boot');
 
 module.exports = (value) => {
 	return String(value).toUpperCase();
