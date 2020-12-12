c71e0d71624d2773512e8827d5cf6de76413d0b5
hanul <hanul@hanul.me>
2020-12-13 02:02:08 +0900

    노드 모듈 업데이트

---
+++ node_modules/uppercase/node_modules/sift/test/immutable-test.js
@@ -0,0 +1,13 @@
+// describe(__filename + "#", function() {
+//   var topic = Immutable.List([1, 2, 3, 4, 5, 6, 6, 4, 3]);
+
+//   var persons = Immutable.fromJS([
+//     { person: { age: 3 } },
+//     { person: { age: 5 } },
+//     { person: { age: 8 } }
+//   ]);
+
+//   xit("works with Immutable.Map in a Immutable.List", function() {
+//     assert.equal(persons.filter(sift({ "person.age": { $gt: 4 } })).size, 2);
+//   });
+// });
